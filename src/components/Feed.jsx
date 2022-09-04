f4d03ac56ca31303c2c5a52cfe082e7f3413d2cd
rabobahago <rabobahago@gmail.com>
2022-09-04 14:40:44 +0100

    sidebar features added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,26 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
-
-const Feed = () => (
-  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box
-      sx={{
-        height: { sx: "auto", md: "92vh" },
-        borderRight: "1px solid #3d3d3d",
-        p: { sx: 0, md: 2 },
-      }}
-    >
-      Sidebar
-      <Typography
-        className="copyright"
-        variant="body2"
-        sx={{ mt: 1.5, color: "#fff" }}
+import { Sidebar } from ".";
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
       >
         <Sidebar />
-        copyright 2022 Rabo Yusuf
-      </Typography>
-    </Box>
-  </Stack>
-);
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright © 2022 Rabo Yusuf
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        ></Typography>
+      </Box>
+    </Stack>
+  );
+};
+
 export default Feed;
